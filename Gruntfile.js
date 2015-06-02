3b09dec47c102e7c89d88594901a239b859157ca
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-06-02 10:02:19 +0200

    grunt cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,37 @@
 module.exports = function(grunt) {
-
+  
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
+  
   grunt.initConfig({
-    open : {
-      dev: {
-        path: 'http://localhost:1919'
-      }
+    
+    clean: {
+      build: ["demo_docs/build"],
+      fonts: ["sphinx_rtd_theme/static/fonts"]
     },
-
-    connect: {
-      server: {
-        options: {
-          port: 1919,
-          base: 'demo_docs/build',
-          livereload: true
-        }
-      }
-    },
+    
     copy: {
       fonts: {
-        files: [
-          // includes files within path
-          {expand: true, flatten: true, src: ['bower_components/font-awesome/fonts/*'], dest: 'sphinx_rtd_theme/static/fonts/', filter: 'isFile'}
-        ]
+        files: [{ // includes files within path
+	  expand: true,
+	  flatten: true,
+	  src: ['bower_components/font-awesome/fonts/*'],
+	  dest: 'sphinx_rtd_theme/static/fonts/',
+	  filter: 'isFile'
+	}]
       }
     },
-
+    
     sass: {
       dev: {
         options: {
           style: 'expanded',
-          loadPath: ['bower_components/bourbon/dist', 'bower_components/neat/app/assets/stylesheets', 'bower_components/font-awesome/scss', 'bower_components/wyrm/sass']
+          loadPath: [
+	    'bower_components/bourbon/dist',
+	    'bower_components/neat/app/assets/stylesheets',
+	    'bower_components/font-awesome/scss',
+	    // 'bower_components/wyrm/sass'
+	  ]
         },
         files: [{
           expand: true,
@@ -45,7 +44,12 @@ module.exports = function(grunt) {
       build: {
         options: {
           style: 'compressed',
-          loadPath: ['bower_components/bourbon/dist', 'bower_components/neat/app/assets/stylesheets', 'bower_components/font-awesome/scss', 'bower_components/wyrm/sass']
+          loadPath: [
+	    'bower_components/bourbon/dist',
+	    'bower_components/neat/app/assets/stylesheets',
+	    'bower_components/font-awesome/scss',
+	    // 'bower_components/wyrm/sass'
+	  ]
         },
         files: [{
           expand: true,
@@ -56,7 +60,7 @@ module.exports = function(grunt) {
         }]
       }
     },
-
+    
     exec: {
       bower_update: {
         cmd: 'bower update'
@@ -65,23 +69,35 @@ module.exports = function(grunt) {
         cmd: 'sphinx-build demo_docs/source demo_docs/build'
       }
     },
-    clean: {
-      build: ["demo_docs/build"],
-      fonts: ["sphinx_rtd_theme/static/fonts"]
+    
+    connect: {
+      server: {
+        options: {
+          port: 1919,
+          base: 'demo_docs/build',
+          livereload: true
+        }
+      }
     },
-
+    
+    open : {
+      dev: {
+        path: 'http://localhost:1919'
+      }
+    },
+    
     watch: {
-      /* Compile sass changes into theme directory */
+      // Compile sass changes into theme directory
       sass: {
         files: ['sass/*.sass', 'bower_components/**/*.sass'],
         tasks: ['sass:dev']
       },
-      /* Changes in theme dir rebuild sphinx */
+      // Changes in theme dir rebuild sphinx
       sphinx: {
         files: ['sphinx_rtd_theme/**/*', 'demo_docs/**/*.rst', 'demo_docs/**/*.py'],
-        tasks: ['clean:build','exec:build_sphinx']
+        tasks: ['clean:build', 'exec:build_sphinx']
       },
-      /* live-reload the demo_docs if sphinx re-builds */
+      // live-reload the demo_docs if sphinx re-builds
       livereload: {
         files: ['demo_docs/build/**/*'],
         options: { livereload: true }
@@ -89,7 +105,7 @@ module.exports = function(grunt) {
     }
 
   });
-
+  
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -97,9 +113,26 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-open');
-
-  grunt.registerTask('fonts', ['clean:fonts','copy:fonts']);
-  grunt.registerTask('default', ['exec:bower_update','clean:build','sass:dev','exec:build_sphinx','connect','open','watch']);
-  grunt.registerTask('build', ['exec:bower_update','clean:build','sass:build','exec:build_sphinx']);
+  
+  grunt.registerTask('fonts', [
+    'clean:fonts',
+    'copy:fonts'
+  ]);
+  
+  grunt.registerTask('default', [
+    // 'exec:bower_update',
+    'clean:build',
+    'sass:dev',
+    'exec:build_sphinx',
+    'connect',
+    'open',
+    'watch'
+  ]);
+  
+  grunt.registerTask('build', [
+    'exec:bower_update',
+    'clean:build',
+    'sass:build',
+    'exec:build_sphinx'
+  ]);
 }
-
