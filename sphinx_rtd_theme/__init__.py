96deead71a79d924e2d3b3f5989a4123953cb88a
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-06-02 10:01:24 +0200

    sphinx_rtd_theme cleanup

--- sphinx_rtd_theme/__init__.py
+++ sphinx_rtd_theme/__init__.py
@@ -3,6 +3,7 @@
 From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 
 """
+
 import os
 
 VERSION = (0, 1, 8)
@@ -10,8 +11,6 @@ VERSION = (0, 1, 8)
 __version__ = ".".join(str(v) for v in VERSION)
 __version_full__ = __version__
 
-
 def get_html_theme_path():
     """Return list of HTML theme paths."""
-    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    return cur_dir
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
